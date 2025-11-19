420496e465871a5906e962fabce0bdaf14dedf98
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-11-19 15:45:43 -0500

    Completed Creature class stuff

--- index.js
+++ index.js
@@ -5,7 +5,18 @@
  */
 
 class Creature {
-  // write your code here
+  constructor(name, habitat) {
+    this.name = name;
+    this.habitat = habitat;
+  }
+
+  describe() {
+    return `${this.name} lives in the ${this.habitat}.`;
+  }
+
+  sleep() {
+    return `${this.name} falls asleep in the ${this.habitat}. 😴`;
+  }
 }
 
 /**
@@ -15,7 +26,14 @@ class Creature {
  */
 
 class Dragon extends Creature {
- //write your code here
+  constructor(name, habitat, firePower) {
+    super(name, habitat);
+    this.firePower = firePower;
+  }
+
+  specialMove() {
+    return `${this.name} breathes fire with ${this.firePower} intensity! 🔥`;
+  }
 }
 
 /**
@@ -25,8 +43,17 @@ class Dragon extends Creature {
  */
 
 class Unicorn extends Creature {
-  //write your code here
+  constructor(name, habitat, sparkleLevel) {
+    super(name, habitat);
+    this.sparkleLevel = sparkleLevel;
+  }
+
+  specialMove() {
+    return `${this.name} heals allies with a sparkle level of ${this.sparkleLevel}! ✨`;
+  }
 }
+
+const jeff = new Unicorn('Rosie', 'Mountains', 'Lava');
 
 // Export the classes for testing
 module.exports = { Creature, Dragon, Unicorn };
